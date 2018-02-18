7c480575ca1024770c5864d2ab25014d1981e9cd
Alex Machado <amachado@sva.edu>
2018-02-18 12:03:47 -0500

    change brush color on key typed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
+var value = [255, 224, 250];
+
 function setup(){
   createCanvas(600,400);
 }
 
 function draw(){
   if (mouseIsPressed){
-    fill(255, 224, 250);
+    fill(value);
     noStroke();
     ellipse(mouseX, mouseY, 10, 10);
   } else {}
+}
+
+function keyTyped(){
+  if (key === 'v'){
+    value = [255, 148, 0];
+  } else if (key === 'b'){
+    value = [255, 224, 250];
+  } else if (key === 'c'){
+    background(255, 255, 255);
+  }
 }
